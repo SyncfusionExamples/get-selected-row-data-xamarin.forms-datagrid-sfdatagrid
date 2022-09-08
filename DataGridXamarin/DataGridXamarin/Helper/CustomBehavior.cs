765d3f67dcef3c0e881504243f4d714262430c72
lakshmi.natarajan <lakshmi.natarajan@syncfusion.com>
2022-09-08 16:43:41 +0530

    XAMARIN:5101 sample committed

---
+++ DataGridXamarin/DataGridXamarin/Helper/CustomBehavior.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.SfDataGrid.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DataGridXamarin
+{
+    public class CustomBehavior:Behavior<SfDataGrid>
+    {
+        #region Overrides
+        protected override void OnAttachedTo(SfDataGrid bindable)
+        {
+            bindable.SelectionChanged += Bindable_SelectionChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        private void Bindable_SelectionChanged(object sender, GridSelectionChangedEventArgs e)
+        {
+            OrderInfo selectedBatchRecord = (e.AddedItems[0] as OrderInfo);
+        }
+
+        protected override void OnDetachingFrom(SfDataGrid bindable)
+        {
+            bindable.SelectionChanged -= Bindable_SelectionChanged;
+            base.OnDetachingFrom(bindable);
+        }
+        #endregion
+    }
+}
